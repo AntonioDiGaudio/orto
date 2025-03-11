74cbd6f22a95bc8427a0e6a25c1353e7e2cac505
AntonioDiGaudio <digaudio.antonio53@gmail.com>
2025-03-11 13:58:24 +0100

    Update carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,25 @@
-
+// Inizializza lo Swiper come al solito
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal', // Puoi scegliere 'horizontal' o 'vertical' per il tipo di scorrimento
-    loop: true, // Loop continuo
-    pagination: {
-      el: '.swiper-pagination', // Se vuoi la paginazione
-    },
-    navigation: {
-      nextEl: '.swiper-button-next', // Pulsante next
-      prevEl: '.swiper-button-prev', // Pulsante prev
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar', // Se desideri aggiungere un scrollbar
-    },
-    autoplay: {
-        delay: 8000,
-      },
-      effect: 'cube',
-  cubeEffect: {
-    slideShadows: false,
+  direction: 'horizontal',
+  loop: false,  
+  pagination: {
+    el: '.swiper-pagination', 
+    type: 'fraction',
   },
-  });
-  
+  effect: 'coverflow', 
+  coverflowEffect: {
+    rotate: 60,  
+    stretch: 0,  
+    depth: 50,  
+    modifier: 1, 
+    slideShadows: false, 
+  },
+  allowTouchMove: true,
+  slidesPerView: 3, 
+  spaceBetween: 0, 
+  centeredSlides: true, 
+  initialSlide: 1,
+});
+
+
+
